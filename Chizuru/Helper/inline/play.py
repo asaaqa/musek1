8858c1f18acf644b607c5d70827ed5cfb6e012bd
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-19 19:42:57 +0530

    Update play.py

--- Chizuru/Helper/inline/play.py
+++ Chizuru/Helper/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from Chizuru.utils.formatters import time_to_seconds
+from Chizuru.Helper.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
